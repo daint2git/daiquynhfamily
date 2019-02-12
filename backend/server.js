d80e92def7376656313c5d44c644e4d893a3cb90
daint2git <33870360+daint2git@users.noreply.github.com>
2019-02-12 14:22:41 +0700

    Update server.js
    Modify path

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,13 @@ const origin =
   process.env.NODE_ENV === 'production'
     ? 'https://daiquynhfamily.herokuapp.com'
     : 'http://localhost:6969'
+const outputPath = path.resolve(process.cwd(), '../frontend/build')
 
 app.use(cors({ credentials: true, origin }))
 app.use(cookieParser())
 
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(outputPath))
 
 const handleClientCall = folderId => (req, res) => {
   const params = { folderId }
@@ -46,6 +47,6 @@ app.get('/api/chupcuoile', handleClientCall('1X4vLvMCyXBM_iVQKOWg8m_y4JvwwPxB5')
 app.get('/api/chupcuoitiec', handleClientCall('1KU7pMSg8VnQdQ7bUw5MvEZwckoVIhyB-'))
 
 // Handles any requests that don't match the ones above
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
+app.get('*', (req, res) => res.sendFile(path.resolve(outputPath, 'index.html')))
 
 app.listen(port, () => console.log(`Server listening on port ${port}`))
